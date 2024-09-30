a48f68b4eb864349831756b91eadf299f003100a
Kartikhub <sharmakartik66@gmail.com>
2024-09-30 22:20:50 +0530

    add wallet adapter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 import './App.css'
-import { TokenLaunchPad } from './components/TokenLaunchPad'
+import { TokenLaunchPad } from './components/TokenLaunchPad';
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 function App() {
-
+	const url = import.meta.env.VITE_RPC_URL;
   return (
-    <div>
-      <div>
-        
-      </div>
-      <TokenLaunchPad />
-    </div>
+    // <div>
+        <ConnectionProvider endpoint={url}>
+					<WalletProvider wallets={[]} autoConnect>
+						<WalletModalProvider>
+										
+						 <div style={{
+								width: "90vw",
+								display: "flex",
+								justifyContent: "flex-end",
+								paddingTop: "8px",
+							}}>
+								<WalletMultiButton />
+							</div>
+							<TokenLaunchPad />
+						</WalletModalProvider>
+					</WalletProvider>
+				</ConnectionProvider>
+      // </div>
+
+    // </div>
   )
 }
 
